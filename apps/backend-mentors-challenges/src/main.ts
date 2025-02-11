7297555d56bb190eec31b4dce4b715a60bd23799
Daniel Montes <gdanielmontesh9@gmail.com>
2025-02-11 11:48:00 -0600

    feat: :sparkles: add cors config

--- apps/backend-mentors-challenges/src/main.ts
+++ apps/backend-mentors-challenges/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: [],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
