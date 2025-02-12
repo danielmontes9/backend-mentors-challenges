5e6f703a6e26c4f5a690e1c0d2b36bc908c0aec8
Daniel Montes <gdanielmontesh9@gmail.com>
2025-02-12 12:24:00 -0600

    feat: :sparkles: add config to app module

--- apps/backend-mentors-challenges/src/app.module.ts
+++ apps/backend-mentors-challenges/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdviceGeneratorService } from 'apps/advice-generator/src/advice-generator.service';
 import { AdviceGeneratorController } from 'apps/advice-generator/src/advice-generator.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+  ],
   controllers: [AppController, AdviceGeneratorController],
   providers: [AppService, AdviceGeneratorService],
 })
--- apps/backend-mentors-challenges/src/app.service.ts
+++ apps/backend-mentors-challenges/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+
+  getPort(): number {
+    return this.configService.get<number>('PORT', 3000);
+  }
+
+  getUNSPLASH_URL(): string {
+    return this.configService.get<string>('UNSPLASH_URL');
   }
 }
