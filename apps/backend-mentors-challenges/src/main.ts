1d23589e45a53b7bf579f452e1d1496e304e0929
Daniel Montes <gdanielmontesh9@gmail.com>
2025-02-13 11:45:00 -0600

    feat: :sparkles: config to use swagger

--- apps/backend-mentors-challenges/src/main.ts
+++ apps/backend-mentors-challenges/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       credentials: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Ejemplo')
+    .setDescription('Documentación de la API usando Swagger')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
