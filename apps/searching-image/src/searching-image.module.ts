66341ba49670fbcb8352f0d98a9d8c8bc8e308df
Daniel Montes <gdanielmontesh9@gmail.com>
2025-02-25 21:02:04 -0600

    feat: :sparkles: add throttler in search image module

--- apps/searching-image/src/searching-image.module.ts
+++ apps/searching-image/src/searching-image.module.ts
@@ -4,14 +4,29 @@ import { SearchingImageService } from './searching-image.service';
 import { ClientIdInterceptor } from './core/interceptors/client-id.interceptor';
 import { HttpModule } from '@nestjs/axios';
 import { HttpErrorsInterceptor } from './core/interceptors/http-errors.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 3,
+        },
+      ],
+    }),
+    HttpModule,
+  ],
   controllers: [SearchingImageController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: HttpErrorsInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
     ClientIdInterceptor,
     SearchingImageService,
